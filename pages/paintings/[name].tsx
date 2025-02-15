d866af19fdbc5e4111c8ec0282cfca8a35b3c814
neolink78 <thibaut.resse@outlook.fr>
2025-02-15 05:33:49 +0100

    prepared painting id's webpage, need to do context for cart

--- pages/paintings/[name].tsx
+++ pages/paintings/[name].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import LeftArrow from "@/lib/icons/leftArrow";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import * as PaintingsService from '@/services/paintings.service'
 import Image from "next/image";
 
@@ -11,36 +11,53 @@ type PaintingType = {
    technique: string
    category: string
    price: string
+   description: string
    
 }
 
 const PaintingId = () => {
     const router=useRouter()
-    const [painting, setPainting] = useState()
+    const [painting, setPainting] = useState<PaintingType>()
     
-    const fetchPainting = async () => {
+    const fetchPainting = useCallback(async () => {
       if (!router.query.id) return
       const paint = await PaintingsService.getById(Number(router.query.id))
       setPainting(paint)
-   }
-   
+   },[router.query])
+
    useEffect(() => {
       fetchPainting()
    },[])
+   
 
 
-
+console.log(painting)
  return painting && (
- <>
-    <div onClick={() => router.push('/galery')} className="hover:cursor-pointer"><LeftArrow /></div>
-    <div className="flex gap-8 ml-8">
-     <Image src={painting.picture} width={300} height={300} alt={painting.name} />
-     <div className="flex flex-col items-center">
+ <div className="mx-[15rem]"> 
+    <div onClick={() => router.push('/galery')} 
+    className="flex gap-2 items-center justify-start hover:cursor-pointer w-fit hover:border-b-2 hover:border-black border-b-2 border-transparent"
+    >
+      <LeftArrow />
+      Retourner dans la galerie
+    </div>
+    <div className="flex justify-center gap-2 mt-2">
+      <div className=" w-[30%] h-[40rem] relative">
+     <Image 
+     src={painting.picture}  
+     fill
+     objectPosition="top"
+     objectFit="contain"
+     alt={painting.name} />
+     </div>
+     <div className="flex flex-col items-center w-[30%]">
     <p className="text-[2rem]">{painting.name}</p>
+    <p className="text-[1.5rem]">{painting.category}</p>
+    <p className="text-[1.5rem]">{painting.technique}</p>
     <p className="mt-8">{painting.description}</p>
+    <p className="mt-8">{painting.price}€</p>
      </div>
      </div>
- </>
+ </div>
  )
 }
 
