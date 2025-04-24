29876709eabf294a84d10512faabe26a3e2cba90
neolink78 <thibaut.resse@outlook.fr>
2025-04-24 12:27:27 +0200

    added Head name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import Layout from '@/components/layout';
 import { CartProvider } from '@/context/cartContext';
 import '@/styles/globals.css'
 import { AppProps } from 'next/app';
-
+import Head from 'next/head';
 
 function App({Component, pageProps }: AppProps) {
     return (
         <CartProvider>
         <Layout>
+        <Head>
+        <title>Atelier de Bernard</title>
+      </Head>
         <Component {...pageProps} />
         </Layout>
         </CartProvider>
